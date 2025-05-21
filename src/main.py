e08b669b5168d65e8d38f7993ef3395cc1e487c6
Paula Barillas <106341373+paulabaal12@users.noreply.github.com>
2025-05-21 11:28:29 -0600

    pruebas

--- src/main.py
+++ src/main.py
@@ -17,15 +17,15 @@ DATA_PATH = Path('../data/processed/')
 
 GENRE_MAP = {
     0: "blues",
-    1: "classical",
-    2: "country",
-    3: "disco",
-    4: "hiphop",
-    5: "jazz",
-    6: "metal",
-    7: "pop",
-    8: "reggae",
-    9: "rock"
+    1: "jazz",
+    2: "pop",
+    3: "reggae",
+    4: "metal",
+    5: "disco",
+    6: "classical",
+    7: "hiphop",
+    8: "rock",
+    9: "country"
 }
 
 def load_models():
